b93b9d690d06455dbded989ded7ac965844bba13
ljs5960 <limjs2671@gmail.com>
2024-01-18 20:18:14 +0900

    Annotaions(1/2)

--- TypeScript/workspace/src/index.ts
+++ TypeScript/workspace/src/index.ts
@@ -1,7 +1,13 @@
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
+import TodoItem from "./TodoItem";
+
+const data = [
+    {id: 1, task: '장보기', complete: true},
+    {id: 2, task: '공부하기', complete: false},
+]
+
+console.log('My Todo List');
+
+for (let i = 0; i < data.length; i++) {
+    let todoItem = new TodoItem(data[i].id, data[i].task, data[i].complete);
+    todoItem.printDetails();
+}
