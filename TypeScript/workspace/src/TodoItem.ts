229ececccaaf6c802f09e5b637d59f66958b3b88
ljs5960 <limjs2671@gmail.com>
2024-01-18 21:21:22 +0900

    Annotations(2/2)

--- TypeScript/workspace/src/TodoItem.ts
+++ TypeScript/workspace/src/TodoItem.ts
@@ -1,10 +1,10 @@
 class TodoItem {
-    constructor(public id: number, public task: string, public complete: boolean) {
+    constructor (public id: number, public task: string, public complete:  boolean = false) {
         this.id = id;
         this.task = task;
         this.complete = complete;
     }
-
+    
     printDetails(): void {
         console.log(`${this.id}\t${this.task}\t${this.complete ? '\t(complete)':''}`);
     }
