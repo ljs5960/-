f1f1f33863c36c41c76622115f4d47b2be92cc08
ljs5960 <limjs2671@gmail.com>
2024-01-21 15:53:17 +0900

    Add Node.js Study Files 02

--- Do it! Node.js 프로그래밍 입문/doit-node-main/myContacts/config/dbConnect.js
+++ Do it! Node.js 프로그래밍 입문/doit-node-main/myContacts/config/dbConnect.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// DB 접속시 비동기 처리 필요
+// async, await 방식 사용
 const dbConnect = async() => {
     try {
-        const connect = await mongoose.connect(process.env.DB_CONNECT);
+        // DB 접속시 시간소요되므로 await
+        const connect = await mongoose.connect(process.env.DB_CONNECT); 
         console.log('DB Connected!');
     } catch(err) {
         console.log(err);
---
+++ Do it! Node.js 프로그래밍 입문/doit-node-main/myContacts/models/contactModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const contactsShcema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String
+        },
+        phone: {
+            type: String,
+            required: [true, "Please Input Your Phone."]
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+// 스키마 -> 모델
+// mongoose.model(<Model명>, <스키마함수>);
+const Contact = mongoose.model("Contact", contactsShcema);
+
+// module로 export하기 위해 const Contact로 변수에 할당
+moduls.exports = Contact;
