133f9890d79d9f4cef267bb43e26d4696c0413d0
ljs5960 <limjs2671@gmail.com>
2024-01-30 21:54:32 +0900

    Study Node.js

---
+++ 따라하며 배우는 노드, 리액트 시리즈/boiler-plate/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const config = require('./key');
+const {User} = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected!')).catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/register', async(req, res) => {
+    const user = new User(req.body);
+
+    try {
+        return res.status(200).json({success: true});
+    } catch(err) {
+        return res.json({success: false, err});
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
