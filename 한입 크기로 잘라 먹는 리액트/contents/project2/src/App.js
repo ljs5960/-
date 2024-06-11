b6e70064ac9bd43c16ae765ca29b80b8bf18f1f6
ljs5960 <limjs2671@gmail.com>
2024-06-11 20:55:18 +0900

    Upload '07. useReducer의 상태 관리' Contents Files

--- 한입 크기로 잘라 먹는 리액트/contents/project2/src/App.js
+++ 한입 크기로 잘라 먹는 리액트/contents/project2/src/App.js
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react';
+import {useRef, useReducer} from 'react';
 import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
@@ -109,32 +109,52 @@ const mockTodo = [
     }
 ]
 
+function reducer(state, action) {
+    switch(action.type) {
+        case 'CREATE':
+            return [action.newItem, ...state];
+        case 'UPDATE': {
+            return state.map((item) =>
+                item.id === action.targetId ? {...item, isDone: !item.isDone} : item
+            );
+        }
+        case 'DELETE':
+            return state.filter((item) => item.id !== action.targetId);
+        default:
+            return state;
+    }
+}
+
 function App() {
     const idRef = useRef(17);
-    const [todo, setTodo] = useState(mockTodo);
+    const [todo, dispatch] = useReducer(reducer, mockTodo);
 
     const onCreate = (content) => {
-        const newItem = {
-            id: 0,
-            content,
-            isDone: false,
-            createDate: new Date().getTime(),
-        };
-        setTodo([newItem, ...todo]);
+        dispatch({
+            type: 'CREATE',
+            newItem: {
+                id: idRef.current,
+                content,
+                isDone: false,
+                createDate: new Date().getTime(),
+            },
+        });
         idRef.current += 1;
     };
 
     const onUpdate = (targetId) => {
-        setTodo(
-            todo.map((it) =>
-                it.id === targetId ? { ...it, isDone: !it.isDone } : it
-            )
-        );
+        dispatch({
+            type: 'UPDATE',
+            targetId,
+        });
     };
 
     const onDelete = (targetId) => {
-        setTodo(todo.filter((it) => it.id !== targetId));
-    }
+        dispatch({
+            type: 'DELETE',
+            targetId,
+        });
+    };
 
     return (
         <div className="App">
