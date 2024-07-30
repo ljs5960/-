ea35515e10e788f526cd912612546e2f9f5836a4
ljs5960 <limjs2671@gmail.com>
2024-07-30 22:04:45 +0900

    Create 'project3 [감정 일기장]만들기' contents files

--- 한입 크기로 잘라 먹는 리액트/contents/project3/src/App.js
+++ 한입 크기로 잘라 먹는 리액트/contents/project3/src/App.js
@@ -1,11 +1,97 @@
-import {Link, Route, Routes} from "react-router-dom";
+import {useEffect, useReducer, useRef} from 'react';
+import { Route, Routes} from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 
+const mockData = [
+    {
+        id: 'mock1',
+        date: new Date().getTime(),
+        content: 'mock1',
+        emotionId: 1,
+    },
+    {
+        id: 'mock2',
+        date: new Date().getTime(),
+        content: 'mock2',
+        emotionId: 2,
+    },
+    {
+        id: 'mock3',
+        date: new Date().getTime(),
+        content: 'mock3',
+        emotionId: 3,
+    },
+]
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INIT': {
+            return action.data;
+        }
+        case 'CREATE': {
+            return [action.data, ...state];
+        }
+        case 'UPDATE': {
+            return state.map((it) =>
+                String(it.id) === String(action.data.id) ? { ...action.data} : it
+            );
+        }
+        case 'DELETE': {
+            return state.filter((it) => String(it.id) !== String(action.targetId));
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
 function App() {
+    const [data, dispatch] =useReducer(reducer, []);
+    const idRef = useRef(0);
+
+    useEffect(() => {
+        dispatch({
+            type: 'INIT',
+            data: mockData,
+        });
+    }, []);
+
+    const onCreate = (date, content, emotionId) => {
+        dispatch({
+            type: 'CREATE',
+            data: {
+                id: idRef.current,
+                date: new Date(date).getTime(),
+                content,
+                emotionId,
+            },
+        });
+        idRef.current += 1;
+    };
+
+    const onUpdate = (targetId, date, content, emotionId) => {
+        dispatch({
+            type: 'UPDATE',
+            data: {
+                id: targetId,
+                date: new Date(date).getTime(),
+                content,
+                emotionId,
+            },
+        });
+    };
+
+    const onDelete = (targetId) => {
+        dispatch({
+            type: 'DELETE',
+            targetId,
+        });
+    };
+
     return (
         <div className="App">
             <Routes>
@@ -14,12 +100,6 @@ function App() {
                 <Route path="/diary/:id" element={<Diary />} />
                 <Route path="/edit" element={<Edit />} />
             </Routes>
-            <div>
-                <Link to={"/"}>Home</Link>
-                <Link to={"/new"}>New</Link>
-                <Link to={"/diary"}>Diary</Link>
-                <Link to={"/edit"}>Edit</Link>
-            </div>
         </div>
     );
 }
